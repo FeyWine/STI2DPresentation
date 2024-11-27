a4b243e6d13c624c00ad4b47ed8e95edb5624575
FeyWine <160754297+FeyWine@users.noreply.github.com>
2024-11-27 13:28:08 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
 document.addEventListener('wheel', function(event) {
     if (event.ctrlKey) {
-        event.preventDefault(); // Empêche le zoom avec Ctrl + molette
+        event.preventDefault(); 
     }
 }, { passive: false });
+
+if (!('scrollBehavior' in document.documentElement.style)) {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+          window.scrollTo({
+            top: target.offsetTop,
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  }
+
+
+
